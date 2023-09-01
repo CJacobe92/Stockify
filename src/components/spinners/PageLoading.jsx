dba88cbd6185f23c333a9eb6f3488d72977ab88c
CJacobe92 <jacobe.christian92@gmail.com>
2023-09-02 02:54:38 +0800

    updated spinner

--- src/components/spinners/PageLoading.jsx
+++ src/components/spinners/PageLoading.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import pageSpinner from '../../assets/pageSpinner.gif'
+import pagespinner from '../../assets/pagespinner.gif'
 
 const PageLoading = () => {
   return (
-    <img src={pageLoading} alt='Loading...' height='50' width='50' className='bg-white'/>
+    <img src={pagespinner} alt='Loading...' height='50' width='50' className='bg-white'/>
   )
 }
 
