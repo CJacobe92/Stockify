074088268d2f5978ac11261dbf0cd9627aeb6be0
CJacobe92 <jacobe.christian92@gmail.com>
2023-09-07 05:38:58 +0800

    updated search

--- src/components/fieldsets/TransactionsListSearch.jsx
+++ src/components/fieldsets/TransactionsListSearch.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TransactionsListSearch = ({handleChange, input, setInput}) => {
   return (
     <div className='w-full p-2 bg-white rounded-md shadow-md bg-opacity-80 shadow-indigo-500'>
-      <p className='text-xs font-semibold text-indigo-700'>Search Account Number</p>
+      <p className='text-xs font-semibold text-indigo-700'>Search</p>
       <div className='flex flex-row items-center justify-between'>
         <input onChange={handleChange} type="text" name='input'  className='w-full p-1 text-indigo-700 border-2 border-indigo-500 rounded-sm outline-none '/>
         <select onChange={(e)=> setInput({...input, queryBy: e.target.value})}className='p-1 ml-2 text-indigo-900 bg-indigo-100 border border-indigo-700 outline-none' defaultValue={input.queryBy}>
