d981e91adea08fc1ae3750ba1318465886a8d8e4
CJacobe92 <jacobe.christian92@gmail.com>
2023-09-07 20:26:58 +0800

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Landing from './pages/Landing'
 import Register from './pages/auth/Register'
 import Login from './pages/auth/Login'
@@ -29,6 +29,7 @@ const App = () => {
       <Route path={'/api/v1/auth/activate'} element={<Activate />}/>
       <Route path={'/enableotp'} element={<EnableOTP />} />
       <Route path={'/verifyotp'} element={<VerifyOTP />} />
+
       <Route path={'/review'} element={<Review />} />
       <Route path={'/reset'} element={<Reset />}/>
       <Route path={'/api/v1/auth/password_update'} element={<Update />}/>
@@ -36,6 +37,7 @@ const App = () => {
         <Route path={'/portfolio'} element={<Portfolio />}/>
         <Route path={'/transaction'} element={<Transaction />}/>
         <Route path={'/account'} element={<Account />}/>
+        
       </Route> 
       <Route element={<AdminLayout />}>
         <Route path={'/dashboard'} element={<Dashboard/>} />
