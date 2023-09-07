b7ce4b63e6594cf203fc2de996a52c1ba6a21b2a
CJacobe92 <jacobe.christian92@gmail.com>
2023-09-07 09:57:46 +0800

    test

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -5,17 +5,13 @@ import UserList from '../../components/cards/UserList'
 
 const Dashboard = () => {
 
-  const [input, setInput] = useState({email: ''})
 
-  const handleChange = (e) => {
-    setInput({...input, email: e.target.value})
-  }
 
   return (
     <div className='h-[90vh] py-4 flex w-full flex-col'>
 
       <div className='w-full px-4'>
-        <UserList input={input} handleChange={handleChange}/>
+        <UserList/>
       </div>
      
     </div>
